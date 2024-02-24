54dada54e16ca04fa2b9a1387edf180d3189e4a1
shibasis <13601918+shibasis0801@users.noreply.github.com>
2024-02-24 22:24:48 +0530

    start

---
+++ ios/NormalBridgeModule.h
@@ -0,0 +1,12 @@
+//
+//  NormalBridgeModule.h
+//  KotlinBridgeModule
+//
+//  Created by ovd on 24/02/24.
+//
+
+#ifndef NormalBridgeModule_h
+#define NormalBridgeModule_h
+
+
+#endif /* NormalBridgeModule_h */
---
+++ ios/NormalBridgeModule.m
@@ -0,0 +1,8 @@
+//
+//  NormalBridgeModule.m
+//  KotlinBridgeModule
+//
+//  Created by ovd on 24/02/24.
+//
+
+#import <Foundation/Foundation.h>
